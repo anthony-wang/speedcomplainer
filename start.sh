0e5fbaddad4a5e1eaa276b23c086ce29fb6fa695
Anthony Wang <anthony81212@gmail.com>
2018-03-25 22:22:57 +0200

    - Updated comments on usage

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# start this script using the `flock` command
+# flock -xn /home/pi/speedcomplainer/lock.lck -c /home/pi/speedcomplainer/start.sh
+# http://bencane.com/2015/09/22/preventing-duplicate-cron-job-executions/
+
 cd /home/pi/speedcomplainer/
 nohup python2 /home/pi/speedcomplainer/speedcomplainer.py > /home/pi/speedcomplainer/cronoutput.log 2>&1 &
