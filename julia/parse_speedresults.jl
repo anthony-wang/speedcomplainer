0d85b6189dcacf54a960e236fbd528ec24807107
Anthony Wang <anthony81212@gmail.com>
2018-03-29 21:13:53 +0200

    - Added initial Julia scripts to parse data

--- julia/parse_speedresults.jl
+++ julia/parse_speedresults.jl
@@ -15,3 +15,18 @@ fhandle = open("../data/speedresults.csv","r")
 close(fhandle)
 
 println("Parsing speedresults done")
+
+# note the "@df" macro requires StatPlots.jl
+
+StatPlots.plot(raw[:Date], hcat(raw[:DL], raw[:UL], raw[:Ping]),
+    axis=[:l :l :r],
+    seriestype = :line,
+    legend = :bottomright,
+    title = "#VodafoneDE internet speed history",
+    label = ["DL" "UL" "Ping"],
+    linewidth = 1,
+    # smooth = true,
+    # line = :red,
+    xlabel = "Time",
+    ylabel = "Speed")
+# Plots.plot!(raw[:Date], raw[:Ping], axis=[:r])
